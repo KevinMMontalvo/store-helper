d55f9972817e8776d4aeaa04c1be7fe7c2d0d287
Kevin Montalvo V <montalvokevin22@gmail.com>
2022-07-20 00:01:36 -0500

    fix customer screen

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -2,14 +2,13 @@
 	<div class="d-flex align-center justify-center blue-grey fill-height">
 		<v-app-bar app color="primary" dark>
 			<!--			<v-app-bar-nav-icon @click.stop="menuVisible = !menuVisible"></v-app-bar-nav-icon>-->
-			<v-toolbar-title>Titulo</v-toolbar-title>
+			<v-toolbar-title>FINDOU</v-toolbar-title>
 			<v-spacer/>
 			<span class="mr-1">Tienda: </span>
 			<v-select v-model="currentStore" class="store-select pt-6" :items="stores" item-value="id"
 					  item-text="code"></v-select>
 		</v-app-bar>
 		<div class="d-flex flex-column mx-5">
-			{{ JSON.stringify(savedCoupons) }}
 			<div class="d-flex text-h5 text-center mb-5 white--text">Ingrese el nombre del producto a buscar</div>
 			<!--			<v-text-field class="d-flex text-h4" v-model.trim="searchText" color="primary" background-color="white"-->
 			<!--						  type="text" :rules="[]" @click:append-outer="searchProduct" @keydown.enter="searchProduct"-->
@@ -122,7 +121,7 @@
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn color="green darken-1" text @click="showCouponsDialog = false">Cerrar</v-btn>
+					<v-btn color="green darken-1" text @click="showSavedCouponsDialog = false">Cerrar</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-dialog>
